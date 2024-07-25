fa7ad91e101402e6baf85c042cbf40de6b542fde
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-07-25 12:23:04 +0530

    1st

--- coffee-web/src/App.jsx
+++ coffee-web/src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
