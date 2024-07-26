1ec39939aaaef259f5d9d99c418e64de4113397f
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-07-26 19:34:26 +0530

    Done the navbar

--- coffee-web/src/App.jsx
+++ coffee-web/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-    </div>
-  )
-}
+      <Navbar />
 
-export default App
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
