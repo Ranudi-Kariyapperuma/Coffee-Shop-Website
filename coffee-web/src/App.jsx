ab4230663ea90b53307b0477222c31ff43b10c84
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-07-26 23:30:27 +0530

    changes

--- coffee-web/src/App.jsx
+++ coffee-web/src/App.jsx
@@ -5,11 +5,7 @@ import Home from "./components/Home";
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <Navbar/>
-=======
       <Navbar />
->>>>>>> 1ec39939aaaef259f5d9d99c418e64de4113397f
 
       <main>
         <div id="home">
@@ -19,9 +15,5 @@ const App = () => {
     </div>
   );
 };
-<<<<<<< HEAD
-export default App;
-=======
 
 export default App;
->>>>>>> 1ec39939aaaef259f5d9d99c418e64de4113397f
--- coffee-web/src/components/Home.jsx
+++ coffee-web/src/components/Home.jsx
@@ -1,36 +1,27 @@
-<<<<<<< HEAD
-import React from "react";
-import { Button } from "react-scroll";
-import img from "../assets/img/home.png";
-
-const Home = () => {
-    return (
-      <div>
-          <div>
-              <h1>Start your day with a steaming cup of coffee </h1>
-              <p>Boost your productivity and build your mood with a glass of coffee in the morning </p>
-
-              <div>
-                <Button title="ADD TO CARD"/>
-                <Button title="MORE MENU"/>
-              </div>
-          </div> 
-
-          <div>
-             <img src={img} alt="img" />    
-          </div> 
-      </div>
-    )        
-}
-export default Home;    
-=======
 import React from 'react'
+import Button from '../layouts/Button'
+import img from '../assets/img/home.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div>
+            <h1>Start your day with a steaming cup of coffee</h1>
+            <p>Boost your productivity and build your mood with a glass of coffee
+                in the morning.
+            </p>
+
+            <div>
+                <Button title="ADD TO CARD"/>
+                <Button title="MORE MENU"/>
+            </div>
+        </div>
+
+        <div>
+            <img src={img} alt="img" />
+        </div>
+    </div>
   )
 }
 
 export default Home
->>>>>>> 1ec39939aaaef259f5d9d99c418e64de4113397f
