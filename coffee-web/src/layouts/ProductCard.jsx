c7807e55dbad11e3a0c312bb801eea6f2c207023
Ishara_Dias <isharalakshandias@gmail.com>
2024-07-30 00:32:40 +0530

    Done Product Component

---
+++ coffee-web/src/layouts/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {BsStarFill, BsStarHalf} from 'react-icons/bs'
+import Button from "../layouts/Button";
+
+const ProductCard = (props) => {
+    return (
+        <div className="w-full lg:w-1/4 bg-white p-3 rounded-lg">
+            <img className="rounded-lg" src={props.img} alt="img" />
+            <div className="flex flex-col items-center mt-5 gap-3">
+                <h2 className="font-semibold text-xl">{props.title}</h2>  
+                <div className="flex">
+                    <BsStarFill className="text-brightColor" />
+                    <BsStarFill className="text-brightColor" />
+                    <BsStarFill className="text-brightColor" />
+                    <BsStarFill className="text-brightColor" />
+                    <BsStarHalf className="text-brightColor" />
+                </div>
+                <h3 className="font-semibold text-lg">$15.99</h3>
+                <Button title="Add TO CARD" />
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
